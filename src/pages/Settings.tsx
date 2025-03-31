186a2898ef58ae288730b8298819aea07c549a71
misan <misan.0103av@gmail.com>
2025-03-31 02:17:16 -0600

    Added not functional language button to the sidebar, added a toggle for the theme in the side bar, got rid of the theme toggle in settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Box,
   Paper,
@@ -14,32 +14,112 @@ import {
   InputLabel,
   useTheme,
   SelectChangeEvent,
+  IconButton,
+  Drawer,
+  Toolbar,
 } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 import { toggleTheme } from '../store/slices/themeSlice';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Settings: React.FC = () => {
   const { t, i18n } = useTranslation();
   const theme = useTheme();
   const dispatch = useDispatch();
   const themeMode = useSelector((state: RootState) => state.theme.mode);
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleLanguageChange = (event: SelectChangeEvent<string>) => {
     i18n.changeLanguage(event.target.value);
   };
 
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
   return (
     <Box sx={{ p: 3 }}>
-      <Typography variant="h4" gutterBottom>
-        {t('settings.title')}
-      </Typography>
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          edge="start"
+          onClick={handleDrawerToggle}
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h4" gutterBottom>
+          {t('settings.title')}
+        </Typography>
+      </Toolbar>
+
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+      >
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={handleDrawerToggle}
+          onKeyDown={handleDrawerToggle}
+        >
+          <List>
+            {/* Theme Setting */}
+            {/* <ListItem>
+              <ListItemText
+                primary={t('settings.theme')}
+                secondary={t('settings.darkMode')}
+              />
+              <ListItemSecondaryAction>
+                <Switch
+                  edge="end"
+                  checked={themeMode === 'dark'}
+                  onChange={() => dispatch(toggleTheme())}
+                />
+              </ListItemSecondaryAction>
+            </ListItem> */}
+
+            {/* Language Setting */}
+            <ListItem>
+              <ListItemText
+                primary={t('settings.language')}
+                secondary="Select your preferred language"
+              />
+              <ListItemSecondaryAction>
+                <FormControl sx={{ minWidth: 120 }}>
+                  <Select
+                    value={i18n.language}
+                    onChange={handleLanguageChange}
+                    size="small"
+                  >
+                    <MenuItem value="en">English</MenuItem>
+                    <MenuItem value="es">Español</MenuItem>
+                  </Select>
+                </FormControl>
+              </ListItemSecondaryAction>
+            </ListItem>
+
+            {/* Notifications Setting */}
+            <ListItem>
+              <ListItemText
+                primary={t('settings.notifications')}
+                secondary="Enable or disable system notifications"
+              />
+              <ListItemSecondaryAction>
+                <Switch edge="end" defaultChecked />
+              </ListItemSecondaryAction>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
 
       <Paper sx={{ mt: 3 }}>
         <List>
           {/* Theme Setting */}
-          <ListItem>
+          {/* <ListItem>
             <ListItemText
               primary={t('settings.theme')}
               secondary={t('settings.darkMode')}
@@ -51,7 +131,7 @@ const Settings: React.FC = () => {
                 onChange={() => dispatch(toggleTheme())}
               />
             </ListItemSecondaryAction>
-          </ListItem>
+          </ListItem> */}
 
           {/* Language Setting */}
           <ListItem>
